aa5b2170ff978dee65ad217efc9c4bd4273c4443
Hubert-mcast <hubert.grima.i84922@mcast.edu.mt>
2026-01-28 21:55:50 +0100

    added firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:home_assignment/pages/weather_page.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   await Hive.initFlutter();
   var box = await Hive.openBox('SavedLocations');
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MainApp());
 }
